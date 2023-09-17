a0de0dd87725dd0243ba5b3d3ed02a5ba7abaec8
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-09-17 23:21:39 +0530

    battery level is updating in db after every second when it changes

--- app/src/main/java/com/example/famsafe/MainActivity.kt
+++ app/src/main/java/com/example/famsafe/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.famsafe
 
-
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -14,19 +17,25 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    private lateinit var batteryPercentageListener: BatteryPercentageListener
     lateinit var binding: ActivityMainBinding
+
+    // Create a BroadcastReceiver to listen for battery changes
+    private val batteryReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == Intent.ACTION_BATTERY_CHANGED) {
+                val batteryPercentage = getBatteryPercentage()
+                updateBatteryPercentageInDatabase(batteryPercentage)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.bottomBar.setOnItemSelectedListener { menuItem ->
-
             when (menuItem.itemId) {
                 R.id.nav_home -> {
                     inflateFragment(HomeFragment())
@@ -39,48 +48,28 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.nav_profile -> {
                     inflateFragment(ProfileFragment())
-
                 }
             }
-
             true
         }
 
         binding.bottomBar.selectedItemId = R.id.nav_home
 
-
-
-
         val currentUser = FirebaseAuth.getInstance().currentUser
-        val name = currentUser?.displayName.toString()
         val mail = currentUser?.email.toString()
-        val phoneNumber = currentUser?.phoneNumber.toString()
-        val imageUrl = currentUser?.photoUrl.toString()
 
+        // Register the BroadcastReceiver to listen for battery changes
+        registerReceiver(batteryReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
-        val db = Firebase.firestore
+        // Initialize the initial battery percentage in the database
+        val batteryPercentage = getBatteryPercentage()
+        updateBatteryPercentageInDatabase(batteryPercentage)
+    }
 
-        val user = hashMapOf(
-            "name" to name,
-            "mail" to mail,
-            "phoneNumber" to phoneNumber,
-            "imageUrl" to imageUrl
-
-        )
-
-
-        db.collection("users").document(mail).set(user).addOnSuccessListener {
-
-        }.addOnFailureListener {
-            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
-   Log.d("testhaibhai",it.toString())
-        }
-
-
-
-
-
-
+    override fun onDestroy() {
+        super.onDestroy()
+        // Unregister the BroadcastReceiver when the activity is destroyed
+        unregisterReceiver(batteryReceiver)
     }
 
     private fun inflateFragment(newInstance: Fragment) {
@@ -88,4 +77,39 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.container, newInstance)
         transaction.commit()
     }
+
+    private fun getBatteryPercentage(): Int {
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            applicationContext.registerReceiver(null, ifilter)
+        }
+
+        val level: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val scale: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+
+        return if (level != -1 && scale != -1) {
+            (level * 100 / scale)
+        } else {
+            -1
+        }
+    }
+
+    private fun updateBatteryPercentageInDatabase(batteryPercentage: Int) {
+        val db = Firebase.firestore
+        val mail = FirebaseAuth.getInstance().currentUser?.email.toString()
+
+        val user = hashMapOf("battery" to batteryPercentage.toString())
+
+        // Explicitly cast the user HashMap to the required Map type
+        val userMap: Map<String, Any> = user
+
+        db.collection("users").document(mail).update(userMap)
+            .addOnSuccessListener {
+                // Successfully updated the battery percentage in the database
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+                Log.d("testhaibhai", e.toString())
+            }
+    }
+
 }
