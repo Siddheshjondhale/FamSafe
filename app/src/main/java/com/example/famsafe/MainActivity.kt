312584d43f075de4ef420b3cae67e2f5bdd23c93
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-09-18 01:46:11 +0530

    long and latitude fetched

--- app/src/main/java/com/example/famsafe/MainActivity.kt
+++ app/src/main/java/com/example/famsafe/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.example.famsafe
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.BatteryManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import androidx.fragment.app.Fragment
 import com.example.famsafe.databinding.ActivityMainBinding
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+
+    )
+    val permissionCode = 78
+
 
     private lateinit var batteryPercentageListener: BatteryPercentageListener
     lateinit var binding: ActivityMainBinding
@@ -30,10 +51,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //oncreate starts
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        take permissions
+        if (isAllPermissionsGranted()) {
+            if (isLocationEnabled(this)) {
+                setUpLocationListener()
+            } else {
+                showGPSNotEnabledDialog(this)
+            }
+        } else {
+            askForPermission()
+        }
+
+
+
+
 
         binding.bottomBar.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -41,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                     inflateFragment(HomeFragment())
                 }
                 R.id.nav_dashboard -> {
-                    inflateFragment(GuardFragment())
+                    inflateFragment(MapsFragment())
                 }
                 R.id.nav_guard -> {
                     inflateFragment(GuardFragment())
@@ -65,6 +103,108 @@ class MainActivity : AppCompatActivity() {
         val batteryPercentage = getBatteryPercentage()
         updateBatteryPercentageInDatabase(batteryPercentage)
     }
+
+//oncreate ends
+
+    private fun isAllPermissionsGranted(): Boolean {
+        for (item in permissions) {
+            if (ContextCompat
+                    .checkSelfPermission(
+                        this,
+                        item
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    private fun askForPermission() {
+        ActivityCompat.requestPermissions(this, permissions, permissionCode)
+    }
+
+    private fun setUpLocationListener() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    for (location in locationResult.locations) {
+                        Log.d("Location89", "onLocationResult: latitude ${location.latitude}")
+                        Log.d("Location89", "onLocationResult: longitude ${location.longitude}")
+
+
+                        val currentUser = FirebaseAuth.getInstance().currentUser
+                        val mail = currentUser?.email.toString()
+
+                        val db = Firebase.firestore
+
+                        val locationData = mutableMapOf<String,Any>(
+                            "lat" to location.latitude.toString(),
+                            "long" to location.longitude.toString(),
+                        )
+
+
+                        db.collection("users").document(mail).update(locationData)
+                            .addOnSuccessListener {
+
+                            }.addOnFailureListener {
+
+                            }
+
+
+                    }
+                }
+            },
+            Looper.myLooper()
+        )
+    }
+
+    fun isLocationEnabled(context: Context): Boolean {
+        val locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
+    /**
+     * Function to show the "enable GPS" Dialog box
+     */
+    fun showGPSNotEnabledDialog(context: Context) {
+        AlertDialog.Builder(context)
+            .setTitle("Enable GPS")
+            .setMessage("required_for_this_app")
+            .setCancelable(false)
+            .setPositiveButton("enable_now") { _, _ ->
+                context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+            .show()
+    }
+
+
+
 
     override fun onDestroy() {
         super.onDestroy()
